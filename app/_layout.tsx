af9171edd07563489edc0fdfcd2eaa1ca61107ca
Rita Trindade da Cruz <142616617+Ritatrcr@users.noreply.github.com>
2025-05-23 19:15:14 -0500

    actualizacion

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
 import { AuthProvider } from "@/context/authContext/AuthContext";
+import { ClienteProvider } from "@/context/viajeContext/viajeClienteContext";
 import { ViajeProvider } from "@/context/viajeContext/ViajeConductorContext";
 import { Stack } from "expo-router";
 
@@ -6,13 +7,16 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <ViajeProvider>  
-
+      <ViajeProvider> 
+        <ClienteProvider>
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="auth" />
           <Stack.Screen name="(app)" />
+
         </Stack>
-      </ViajeProvider>
+        </ClienteProvider>
+        </ViajeProvider>
+
     </AuthProvider>
   );
 }
